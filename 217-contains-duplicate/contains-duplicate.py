829dbb67ab880f33094545130a7d31130f929bd4
ShreyasMuragodmath <muragodmathshreyas@gmail.com>
2026-01-03 16:43:30 +0100

    Time: 20 ms (45.56%) | Memory: 26.4 MB (6.55%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -4,10 +4,10 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        mp = set()
+        mp = {}
         for i in range(len(nums)):
             if nums[i] in mp:
                 return True
-            mp.add(nums[i])
+            mp[nums[i]] = i
         return False
         
