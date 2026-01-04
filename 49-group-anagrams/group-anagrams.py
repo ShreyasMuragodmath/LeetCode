596df7091ef073b5917dc5668aea660f3f183a20
ShreyasMuragodmath <muragodmathshreyas@gmail.com>
2026-01-04 23:23:14 +0100

    Time: 35 ms (21.92%) | Memory: 18.2 MB (23.35%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.py
@@ -0,0 +1,20 @@
+from collections import defaultdict
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        anagram = defaultdict(list)
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            anagram[tuple(count)].append(s)
+        return anagram.values()
+
+
+
+
+
+
