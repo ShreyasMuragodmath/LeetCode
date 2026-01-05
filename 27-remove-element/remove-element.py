ae73f2dac2e9916931547ac7f3fbeff496505aac
ShreyasMuragodmath <muragodmathshreyas@gmail.com>
2026-01-05 02:08:24 +0100

    Time: 0 ms (100.00%) | Memory: 12.4 MB (82.08%) - LeetSync

---
+++ 27-remove-element/remove-element.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        if len(nums) == 0:
+            return 0
+        elif len(nums) == 1:
+            if nums[0] == val:
+                nums.pop()
+                return 0
+            return 1
+
+        ptr1, ptr2, count = 0, len(nums)-1, 0
+        while(ptr1 != ptr2):
+            if nums[ptr1] == val:
+                count += 1
+                nums[ptr1], nums[ptr2] = nums[ptr2], nums[ptr1]
+                ptr2 = ptr2 - 1
+            else:
+                ptr1 = ptr1 + 1
+        if ptr1 == ptr2:
+            if nums[ptr1] == val:
+                return len(nums) - count - 1
+            
+        return len(nums) - count
+        
