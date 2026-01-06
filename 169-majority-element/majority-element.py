62cfb3640fd6b13ec32ed02cc5054f752374c74d
ShreyasMuragodmath <muragodmathshreyas@gmail.com>
2026-01-06 19:15:48 +0100

    Time: 2 ms (89.98%) | Memory: 13.9 MB (15.36%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -4,9 +4,23 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        mp = {}
-        for i in range (len(nums)):
-            mp[nums[i]] = mp.get(nums[i], 0) + 1
-        return max(mp, key=mp.get)
+        # mp = {}
+        # for i in range (len(nums)):
+        #     mp[nums[i]] = mp.get(nums[i], 0) + 1
+        # return max(mp, key=mp.get)
+
+        count = 0
+        ans = -1
+
+        for curr in nums:
+            if count == 0:
+                ans = curr
+
+            if ans == curr:
+                count += 1
+            else:
+                count -= 1
+
+        return ans
 
         
