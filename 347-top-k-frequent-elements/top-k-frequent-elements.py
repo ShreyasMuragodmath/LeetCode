14c987c42693b015b18457d7436ab0940a062817
ShreyasMuragodmath <muragodmathshreyas@gmail.com>
2026-01-11 23:52:42 +0100

    Time: 23 ms (14.39%) | Memory: 19.9 MB (26.52%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,12 +1,16 @@
 class Solution(object):
     def topKFrequent(self, nums, k):
         mp = {}
+        freq = [[] for i in range(len(nums)+1)]
         for i in range(len(nums)):
             mp[nums[i]] = mp.get(nums[i], 0) + 1
+        for n, c in mp.items():
+            freq[c].append(n)
         
-        ans = []
-        while(k!=0):
-            ans.append(max(mp, key=mp.get))
-            mp.pop(max(mp, key=mp.get))
-            k -= 1
-        return ans
+        res = []
+
+        for i in range(len(freq)-1, 0, -1): # -1 cz we have a 0th index 
+            for n in freq[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
