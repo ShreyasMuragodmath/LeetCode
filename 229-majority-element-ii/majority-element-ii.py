a684226dd7870942c525b1565f076be2f3766a47
ShreyasMuragodmath <muragodmathshreyas@gmail.com>
2026-01-12 23:56:29 +0100

    Time: 6 ms (87.59%) | Memory: 14.2 MB (68.61%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.py
+++ 229-majority-element-ii/majority-element-ii.py
@@ -1,14 +1,45 @@
 class Solution(object):
     def majorityElement(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
-        mp = {}
-        for i in range(len(nums)):
-            mp[nums[i]] = 1 + mp.get(nums[i],0)
+
+        # mp = {}
+        # for i in range(len(nums)):
+        #     mp[nums[i]] = 1 + mp.get(nums[i],0)
+        # res = []
+        # for k, v in mp.items():
+        #     if v > len(nums)//3:
+        #         res.append(k)
+        # return res
+
+        n = len(nums)
+        num1 = num2 = -1
+        cnt1 = cnt2 = 0
+
+        for num in nums:
+            if num == num1:
+                cnt1 += 1
+            elif num == num2:
+                cnt2 += 1
+            elif cnt1 == 0:
+                cnt1 = 1
+                num1 = num
+            elif cnt2 == 0:
+                cnt2 = 1
+                num2 = num
+            else:
+                cnt1 -= 1
+                cnt2 -= 1
+
+        cnt1 = cnt2 = 0
+        for num in nums:
+            if num == num1:
+                cnt1 += 1
+            elif num == num2:
+                cnt2 += 1
+
         res = []
-        for k, v in mp.items():
-            if v > len(nums)//3:
-                res.append(k)
+        if cnt1 > n // 3:
+            res.append(num1)
+        if cnt2 > n // 3:
+            res.append(num2)
+
         return res
