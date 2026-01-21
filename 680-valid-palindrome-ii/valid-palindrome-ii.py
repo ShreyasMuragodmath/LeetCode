b8b3c3324fc32e91657cbae7e0318e0e1bf707d7
ShreyasMuragodmath <muragodmathshreyas@gmail.com>
2026-01-21 20:19:00 +0100

    Time: 77 ms (63.40%) | Memory: 12.8 MB (21.06%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.py
+++ 680-valid-palindrome-ii/valid-palindrome-ii.py
@@ -10,7 +10,6 @@ class Solution(object):
         l, r = 0, len(s) - 1
         while l < r:
             if s[l] != s[r]:
-                # skipL,skipR = s[l + 1: r + 1], s[l: r]
                 return (is_palindrome(l+1, r) or is_palindrome(l,r-1))
             l, r = l + 1, r - 1
         return True
