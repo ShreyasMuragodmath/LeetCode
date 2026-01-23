8a13e3deb13eafda58654b936d636ca5df27e97e
ShreyasMuragodmath <muragodmathshreyas@gmail.com>
2026-01-23 17:28:20 +0100

    Time: 644 ms (82.43%) | Memory: 18.3 MB (90.31%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -3,20 +3,22 @@ class Solution(object):
         res = []
         nums.sort()
         for i, a in enumerate(nums):
-            if i > 0 and a == nums[i-1]:
+            if i > 0 and a == nums[i-1]: # i is not the first element and the element is same as the prev element (A)
                 continue
-            l,r = i+1, len(nums)-1
-            while l<r:
-                threeSum = a+nums[l]+nums[r]
+            l,r = i + 1, len(nums) - 1 # B,C
+            while l < r:
+                threeSum = a + nums[l] + nums[r]
                 if threeSum > 0:
                     r -= 1
                 elif threeSum < 0:
                     l += 1
                 else:
                     res.append([a,nums[l],nums[r]])
-                    l += 1 
+                    # update pointers 
+                    l += 1 #first element of the dublicate elemets
+                           # r updates itself as the sum has now changed (from the above code)
                     while nums[l] == nums[l-1] and l < r:
-                        l+=1
+                        l += 1 #last element of the dublicate elements
         return res
                 
 
