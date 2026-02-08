b5e0944ed46cc941cfd333b6dca9bb185b5e87e8
ShreyasMuragodmath <muragodmathshreyas@gmail.com>
2026-02-08 19:42:29 +0100

    Time: 3 ms (47.00%) | Memory: 12.6 MB (47.20%) - LeetSync

--- 682-baseball-game/baseball-game.py
+++ 682-baseball-game/baseball-game.py
@@ -5,25 +5,16 @@ class Solution(object):
         :rtype: int
         """
         stack = []
-
         for c in operations:
             if c == 'D':
                 stack.append(2 * stack[-1])
             elif c == 'C':
                 stack.pop()
             elif c == '+':
-                a = stack[-1]
-                stack.pop()
-                b = stack[-1]
-                stack.append(a)
-                stack.append(a + b)
+                stack.append(stack[-1] + stack[-2])
             else:
                 stack.append(int(c))
-        res = 0
-        while stack:
-            res += stack[-1]
-            stack.pop()
-        return res
+        return sum(stack)
 
 
 
