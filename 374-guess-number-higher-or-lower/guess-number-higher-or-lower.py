c73a2676b339e37036a3a6c5a7712fdd28df072c
ShreyasMuragodmath <muragodmathshreyas@gmail.com>
2026-02-21 22:32:29 +0100

    Time: 43 ms (56.27%) | Memory: 19.3 MB (24.79%) - LeetSync

--- 374-guess-number-higher-or-lower/guess-number-higher-or-lower.py
+++ 374-guess-number-higher-or-lower/guess-number-higher-or-lower.py
@@ -10,14 +10,31 @@ class Solution:
         left = 0
         right = n
 
-        while left <= right:
-            m = (left + right) // 2
-            pick = guess(m)
-            if pick > 0:
-                left = m + 1
-            elif pick < 0:
-                right = m - 1
+        # while left <= right:
+        #     m = (left + right) // 2
+        #     pick = guess(m)
+        #     if pick > 0:
+        #         left = m + 1
+        #     elif pick < 0:
+        #         right = m - 1
+        #     else:
+        #         return m/
+
+        while True:
+            m1 = left + (right - left) // 3
+            m2 = right - (right - left) // 3
+
+            if guess(m1) == 0:
+                return m1
+            if guess(m2) == 0:
+                return m2
+            if guess(m1) + guess(m2) == 0:
+                left = m1 + 1
+                right = m2 - 1
+            elif guess(m1) == -1:
+                right = m1 - 1
             else:
-                return m
+                left = m2 + 1
+
         
         
